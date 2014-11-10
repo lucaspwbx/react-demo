c7aadc762342ef893ac20be446b1ef11d2a76d88
Lucas Weiblen <lucasweiblen@gmail.com>
2014-11-10 21:25:35 -0200

    first commit

---
+++ app/assets/javascripts/comments.js.jsx
@@ -0,0 +1,67 @@
+/** @jsx React.DOM */
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+          {this.props.comment}
+      </div>
+      );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function () {
+    var commentNodes = this.props.comments.map(function (comment, index) {
+      return (
+        <Comment author={comment.author} comment={comment.comment} key={index} />
+        );
+    });
+
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+      );
+  }
+});
+
+var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {comments: []};
+  },
+  componentDidMount: function () {
+    this.loadCommentsFromServer();
+  },
+  loadCommentsFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function (comments) {
+        this.setState({comments: comments});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function () {
+    return (
+        <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList comments={this.state.comments} />
+        </div>
+      );
+  }
+});
+
+var ready = function () {
+  React.renderComponent(
+      <CommentBox url="/comments.json" />,
+      document.getElementById('comments')
+  );
+};
+
+$(document).ready(ready);
